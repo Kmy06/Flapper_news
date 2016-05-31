2ed14c8f9cd1838e3989b78064f45c7fc3d9fb5b
kmy06 <camille.apheceix@gmail.com>
2016-05-31 10:12:24 +0200

    Routes and Controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+	# pour que les controllers répondent au format json
+	# apres on utlisera respond_with pour que nos actions retournent du json à la fin
+	respond_to :json
 	def angular
 		render 'layouts/application'
 	end
+
 end
---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+# associe post avec comment Un post possède plusieurs commentaires
+	has_many :comments
+end
+# as_json retourne un hash représentant un modèle
+# Super appelle la méthode de la classe parent
+def as_json(options = {})
+	super(options.merge(include: :comments))
+end
